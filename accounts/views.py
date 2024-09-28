337d4911280e76db9f7378c86f5444a499e765e9
adish_ct <adishathu525@gmail.com>
2024-09-28 22:07:46 +0530

    Feat(form added): new form added for user creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def sample(request, *args, **kwargs):
     return HttpResponse(True, 200,)
+
+
+
